519ed1084ca11fecda8b89bb2cd3d763d2e14be5
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-08-23 17:22:24 +0300

    change port

--- src/config/config.js
+++ src/config/config.js
@@ -5,22 +5,24 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-
-    host: "10.0.2.2",
+    host: "127.0.0.1",
     dialect: "mariadb",
+    port: "3307",
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: "10.0.2.2",
+    host: "127.0.0.1",
     dialect: "mariadb",
+    port: "3307",
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: "10.0.2.2",
+    host: "127.0.0.1",
     dialect: "mariadb",
+    port: "3307",
   },
 };
